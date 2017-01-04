f066d70d879f89e8acbbf19419ed088b1c410f38
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-04 23:00:46 +0100

    Added ScaleMode

--- korim-core/src/com/soywiz/korim/geom/ISize.kt
+++ korim-core/src/com/soywiz/korim/geom/ISize.kt
@@ -1,18 +1,24 @@
 package com.soywiz.korim.geom
 
-data class ISize(var width: Int, var height: Int) {
+data class ISize(var width: Int = 0, var height: Int = width) {
 	operator fun contains(v: ISize): Boolean = (v.width <= width) && (v.height <= height)
 
 	operator fun times(v: Double) = ISize((width * v).toInt(), (height * v).toInt())
 
-	fun fitTo(container: ISize): ISize {
-		val ratio1 = container.width.toDouble() / width.toDouble()
-		val ratio2 = container.height.toDouble() / height.toDouble()
-		val size1 = this * ratio1
-		val size2 = this * ratio2
-		val size = if (size1 in container) size1 else size2
-		return size
+	fun setTo(width: Int, height: Int) = this.apply {
+		this.width = width
+		this.height = height
 	}
+
+	fun setTo(that: ISize) = setTo(that.width, that.height)
+
+	fun applyScaleMode(container: ISize, mode: ScaleMode): ISize {
+		return mode(this, container)
+	}
+
+	fun fitTo(container: ISize): ISize = applyScaleMode(container, ScaleMode.SHOW_ALL)
+
+	fun scale(sx: Double, sy: Double = sx) = setTo((this.width * sx).toInt(), (this.height * sy).toInt())
 
 	fun anchoredIn(container: IRectangle, anchor: Anchor): IRectangle {
 		return IRectangle(((container.width - this.width) * anchor.sx).toInt(), ((container.height - this.height) * anchor.sy).toInt(), width, height)
---
+++ korim-core/src/com/soywiz/korim/geom/ScaleMode.kt
@@ -0,0 +1,29 @@
+package com.soywiz.korim.geom
+
+class ScaleMode(private val f: (item: ISize, container: ISize, target: ISize) -> Unit) {
+	operator fun invoke(item: ISize, container: ISize, target: ISize = ISize()): ISize = target.apply {
+		f(item, container, target)
+	}
+
+	companion object {
+		val COVER = ScaleMode { item, container, target ->
+			val s0 = container.width.toDouble() / item.width.toDouble()
+			val s1 = container.height.toDouble() / item.height.toDouble()
+			target.setTo(item).scale(Math.max(s0, s1))
+		}
+
+		val SHOW_ALL = ScaleMode { item, container, target ->
+			val s0 = container.width.toDouble() / item.width.toDouble()
+			val s1 = container.height.toDouble() / item.height.toDouble()
+			target.setTo(item).scale(Math.min(s0, s1))
+		}
+
+		val EXACT = ScaleMode { item, container, target ->
+			target.setTo(container)
+		}
+
+		val NO_SCALE = ScaleMode { item, container, target ->
+			target.setTo(item)
+		}
+	}
+}
