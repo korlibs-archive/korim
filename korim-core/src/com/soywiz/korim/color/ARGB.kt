38361f7fc3391dc062ca166924586e9f527bbe82
soywiz <soywiz@gmail.com>
2017-08-27 22:56:56 +0200

    Added ARGB color format

---
+++ korim-core/src/com/soywiz/korim/color/ARGB.kt
@@ -0,0 +1,8 @@
+package com.soywiz.korim.color
+
+object ARGB : ColorFormat32(), ColorFormatBase by ColorFormatBase.Mixin(
+		bOffset = 24, bSize = 8,
+		gOffset = 16, gSize = 8,
+		rOffset = 8, rSize = 8,
+		aOffset = 0, aSize = 8
+)
