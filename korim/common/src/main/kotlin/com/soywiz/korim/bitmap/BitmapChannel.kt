dc68271dc58e6dcf521fc0684fa1ca122a7902bf
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-12-14 21:31:23 +0100

    Added extra things to BitmapChannel

--- korim/common/src/main/kotlin/com/soywiz/korim/bitmap/BitmapChannel.kt
+++ korim/common/src/main/kotlin/com/soywiz/korim/bitmap/BitmapChannel.kt
@@ -6,4 +6,17 @@ enum class BitmapChannel(val index: Int) {
 	val shift = index * 8
 	val setMask = (0xFF shl shift)
 	val clearMask = setMask.inv()
+
+	fun extract(rgba: Int): Int = (rgba ushr shift) and 0xFF
+	fun insert(rgba: Int, value: Int): Int = (rgba and clearMask) or ((value and 0xFF) shl shift)
+
+	companion object {
+		val ALL = values()
+		operator fun get(index: Int) = ALL[index]
+	}
 }
+
+val BitmapChannel.Companion.Y get() = BitmapChannel.RED
+val BitmapChannel.Companion.Cb get() = BitmapChannel.GREEN
+val BitmapChannel.Companion.Cr get() = BitmapChannel.BLUE
+val BitmapChannel.Companion.A get() = BitmapChannel.ALPHA
