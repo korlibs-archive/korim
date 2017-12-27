858c1d851a6e705a9bf2a9dfb44f1d5db9c9ed55
soywiz <soywiz@gmail.com>
2017-12-27 22:23:15 +0100

    Make PSD standard

--- korim/common/src/main/kotlin/com/soywiz/korim/format/StandardImageFormats.kt
+++ korim/common/src/main/kotlin/com/soywiz/korim/format/StandardImageFormats.kt
@@ -1,5 +1,5 @@
 package com.soywiz.korim.format
 
-val StandardImageFormats = listOf(JPEG, PNG, TGA)
+val StandardImageFormats = listOf(JPEG, PNG, TGA, PSD)
 
 fun ImageFormats.registerStandard() = this.apply { register(StandardImageFormats) }
