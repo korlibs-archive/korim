cf6ab7310d93320b083ad7d93c7cda0e5b609ff4
soywiz <soywiz@gmail.com>
2018-12-30 15:36:51 +0100

    Do not use SubListGeneric from korio

--- korim/src/commonMain/kotlin/com/soywiz/korim/color/RGBA.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/color/RGBA.kt
@@ -1,8 +1,6 @@
 package com.soywiz.korim.color
 
 import com.soywiz.kmem.*
-import com.soywiz.korio.ds.*
-import com.soywiz.korio.error.*
 import com.soywiz.korio.lang.*
 import com.soywiz.korma.interpolation.*
 
@@ -389,7 +387,7 @@ data class RGBA(val rgba: Int) : Comparable<RGBA> {// @TODO: SUPER Extremely slo
 
 //inline class RgbaArray(val array: IntArray) : List<RGBA> { // @TODO: class inline or slow!
 class RgbaArray(val array: IntArray) : List<RGBA> {
-	override fun subList(fromIndex: Int, toIndex: Int): List<RGBA> = SubListGeneric(this, fromIndex, toIndex)
+	override fun subList(fromIndex: Int, toIndex: Int): List<RGBA> = GenericSubList(this, fromIndex, toIndex)
 	override fun contains(element: RGBA): Boolean = array.contains(element.rgba)
 	override fun containsAll(elements: Collection<RGBA>): Boolean = elements.all { contains(it) }
 	override fun indexOf(element: RGBA): Int = array.indexOf(element.rgba)
@@ -422,6 +420,77 @@ class RgbaArray(val array: IntArray) : List<RGBA> {
 }
 
 fun RGBA.mix(other: RGBA, ratio: Double) = RGBA.blendRGBA(this, other, ratio)
+
+// @TODO: Use the one from KDS
+private class GenericSubList<T>(val base: List<T>, val start: Int, val end: Int) : List<T> {
+    init {
+        if (start !in 0..base.size) throw IndexOutOfBoundsException("$start")
+        if (end !in 0..base.size) throw IndexOutOfBoundsException("$end")
+    }
+
+    override val size: Int get() = end - start
+
+    private fun Int.translateIndex(): Int {
+        if (this !in 0 until size) throw IndexOutOfBoundsException("$this")
+        return start + this
+    }
+
+    override fun contains(element: T): Boolean = (0 until size).any { this[it] == element }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        val elementsSet = elements.toMutableSet()
+        for (n in 0 until size) elementsSet -= this[n]
+        return elementsSet.isEmpty()
+    }
+
+    override fun get(index: Int): T = base[index.translateIndex()]
+
+    override fun indexOf(element: T): Int {
+        for (n in 0 until size) if (this[n] == element) return n
+        return -1
+    }
+
+    override fun lastIndexOf(element: T): Int {
+        for (n in size - 1 downTo 0) if (this[n] == element) return n
+        return -1
+    }
+
+    override fun isEmpty(): Boolean = size == 0
+    override fun iterator(): Iterator<T> = GenericListIterator(this)
+    override fun listIterator(): ListIterator<T> = GenericListIterator(this)
+    override fun listIterator(index: Int): ListIterator<T> = GenericListIterator(this, index)
+    override fun subList(fromIndex: Int, toIndex: Int): List<T> = GenericSubList(this, fromIndex, toIndex)
+
+    override fun toString(): String = (0 until size).map { this[it] }.toString()
+}
+
+// @TODO: Use the one from KDS
+private class GenericListIterator<T>(val list: List<T>, val iindex: Int = 0) : ListIterator<T> {
+    init {
+        if (iindex !in 0 until list.size) throw IndexOutOfBoundsException("$iindex")
+    }
+
+    private var index = iindex
+
+    override fun hasNext(): Boolean = index < list.size
+
+    override fun next(): T {
+        if (!hasNext()) throw NoSuchElementException()
+        return list[index++]
+    }
+
+    override fun hasPrevious(): Boolean = index > 0
+
+    override fun nextIndex(): Int = index
+
+    override fun previous(): T {
+        if (!hasPrevious()) throw NoSuchElementException()
+        return list.get(--index)
+    }
+
+    override fun previousIndex(): Int = index - 1
+}
+
 
 /*
 java.lang.VerifyError: Bad type on operand stack
