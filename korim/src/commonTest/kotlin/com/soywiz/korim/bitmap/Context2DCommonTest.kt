cdec8b92c7c35b512a6cdfa22225c212a709f84e
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-05-04 21:06:37 +0200

    Some work on https://github.com/korlibs/korge/issues/127

---
+++ korim/src/commonTest/kotlin/com/soywiz/korim/bitmap/Context2DCommonTest.kt
@@ -0,0 +1,18 @@
+package com.soywiz.korim.bitmap
+
+import com.soywiz.korim.color.*
+import com.soywiz.korma.geom.vector.*
+import kotlin.test.*
+
+class Context2DCommonTest {
+    @Test
+    fun testFillAlpha() {
+        val semiTransparentAlpha = Colors.WHITE.withAd(0.5)
+        val image = NativeImage(10, 10).context2d {
+            fill(semiTransparentAlpha) {
+                rect(-1, -1, 11, 11)
+            }
+        }.toBMP32().depremultipliedIfRequired()
+        assertEquals(semiTransparentAlpha, image[5, 5])
+    }
+}
